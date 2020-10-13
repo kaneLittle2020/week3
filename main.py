81cad56afd13bb3798a062baa41421f23f71e434
kaneLittle2020 <5littk17@solent.ac.uk>
2020-10-13 09:12:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#This is the function 
+def greet_user():
+  print ("please enter your name")
+  name = input ()
+  print ("hello", name)
+
+#calls to the function
+greet_user()
+
---
+++ testfunctions.py
@@ -0,0 +1,8 @@
+#This is the function 
+def greet_user():
+  print ("please enter your name")
+  name = input ()
+  print ("hello", name)
+
+#calls to the function
+greet_user()
