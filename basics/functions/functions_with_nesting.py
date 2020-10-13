5aeaaf7eb5afe38fcf08482dfad224eadf53fb0a
kaneLittle2020 <5littk17@solent.ac.uk>
2020-10-13 09:47:27 +0000

    added the function for response of object

--- basics/functions/functions_with_nesting.py
+++ basics/functions/functions_with_nesting.py
@@ -1,2 +1,16 @@
-print ("What lies before us?")
-obect = input()
+#The function 
+def identify ():
+
+#Asks the user for the what they see
+  print ("What lies before us?")
+  response = input()
+
+  #Shows message
+  if response == "a large boulder" :
+    print ("it's time to run!")
+  else:
+    print ("We will be fine...")
+
+#calls the function
+identify()
+
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+#The function 
+def identify ():
+
+#Asks the user for the what they see
+  print ("What lies before us?")
+  response = input()
+
+  #Shows message
+  if response == "a large boulder" :
+    print ("it's time to run!")
+  else:
+    print ("We will be fine...")
+
+#calls the function
+identify()
+
 
